083e937cf4a3289a79ac03d533cd399b334026d8
malgi <marketa.marki@centrum.cz>
2015-05-31 12:44:50 +0200

    TeacherManager added
    TeacherManagr view adn controller

---
+++ TestingSystem/TestingSystem/Controllers/TeacherManagerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestingSystem.Controllers
+{
+    public class TeacherManagerController : Controller
+    {
+        // GET: TeacherManager
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
